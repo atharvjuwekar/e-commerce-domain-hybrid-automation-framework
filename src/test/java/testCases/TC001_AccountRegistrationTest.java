19914d42cf90755640f3402921a99a998c1c4ae7
atharv <atharvjuwekar.aj@gmail.com>
2024-08-06 21:23:14 +0530

    fixed test conflicts

--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -19,11 +19,7 @@ public class TC001_AccountRegistrationTest extends BaseClass {
 	@Test
 	public void verifyAccountRegistration() {
 
-<<<<<<< HEAD
-		logger.info("************************ Staring TC001_AccountRegistrationTest ************************12");
-=======
-		logger.info("************************ Staring TC001_AccountRegistrationTest ************************#");
->>>>>>> d430247d7522569d2f9b1e5d8802c419ad88382b
+		logger.info("************************ Staring TC001_AccountRegistrationTest ************************");
 
 		try {
 			HomePage hp = new HomePage(driver);
